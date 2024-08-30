d7242a77222e69589191476c2a44a27464feab3d
shard77 <sh7user@gmail.com>
2024-08-30 15:20:28 +0200

    feat(lsp,utils): rustaceanvim config

--- config/plug/utils/rustaceanvim.nix
+++ config/plug/utils/rustaceanvim.nix
@@ -1,3 +1,11 @@
 {
-  plugins.rustaceanvim.enable = true;
+  plugins.rustaceanvim = {
+    enable = true;
+    settings = {
+      tools = {
+        enable_clippy = true;
+        enable_nextest = true;
+      };
+    };
+  };
 }

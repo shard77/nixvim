8128972d2679c764c7fcd1036674d1f8e630168e
shard77 <sh7user@gmail.com>
2024-08-30 10:55:30 +0200

    feat(nixvim): fix cmp

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -26,14 +26,13 @@
           ];
         };
         sources = [
-          { name = "nvim_lsp"; }
-          { name = "emoji"; }
+          {name = "nvim_lsp";}
+          {name = "emoji";}
           {
             name = "buffer"; # text within current buffer
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             keywordLength = 3;
           }
-          { name = "copilot"; }
           {
             name = "path"; # file system paths
             keywordLength = 3;
